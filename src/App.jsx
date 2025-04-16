b19303e98f65e272a0827a5a28d7136e79bee5a6
KalebMorton <kmorton7714@gmail.com>
2025-04-15 19:48:55 -0600

    completed assinement

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import Red from "./components/Red";
+import Blue from "./components/Blue";
+import Home from "./components/Home";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
     <div id="container">
       <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <div id="navbar">
+        <Link to="/blue">blue</Link>
+        <Link to="/red">red</Link>
+        <Link to="/">home</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<Blue/>} />
+          <Route path="/red" element={<Red/>} />
+          <Route path="/" element={<Home/>} />     
+        </Routes> 
+      </div>
     </div>
     </>
   )
